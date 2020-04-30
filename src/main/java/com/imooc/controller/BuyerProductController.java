bec257b3f2f592d43a67485e01e1adabc049aadf
Administrator <760141714@qq.com>
2020-04-30 17:18:07 +0800

    创建产品controller

---
+++ src/main/java/com/imooc/controller/BuyerProductController.java
@@ -0,0 +1,12 @@
+package com.imooc.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 买家controller
+ * @Author Owen Zhao
+ * @Date 2020/4/30
+ */
+@RestController
+public class BuyerProductController {
+}
